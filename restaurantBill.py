1fb9024f27977df81e0fd5ee419887671397577f
Delano Marques <54899930+delanobmarques@users.noreply.github.com>
2023-09-14 15:10:39 -0300

    tech check 1 template

---
+++ restaurantBill.py
@@ -0,0 +1,17 @@
+# Author: Student Name
+# Date: September 14, 2023
+# Title: Restaurant Bill - Teck-check #1
+# Description: Console-based Python program that will calculate the amount of the tip, the tax, and 
+        # the total amount of a restaurant bill. The program will prompt the user to input the original amount
+        #  of the bill and the desired tip. The program will then output: 1) Total cost before tax, 
+        # 2) Tax amount, 3) Tip amount (with percentage entered by the user), 
+        # and 4) Final Cost of the bill, including tax and tip.
+
+def main():#don't change this line
+    # Your code should START here
+
+
+    # Your code should END here
+
+if __name__ == "__main__": #don't change this line
+    main()                  #don't change this line
